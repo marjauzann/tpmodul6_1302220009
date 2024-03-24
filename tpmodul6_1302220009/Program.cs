7c201eba9bdfd1f66e1ed2a54376809fc7dbf7c2
Marjauza Naswansyah <marjauzann@gmail.com>
2024-03-24 13:47:33 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302220009/Program.cs
+++ tpmodul6_1302220009/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace tpmodul6_1302220009
 {
-    internal class Program
+    class Program
     {
+        public class SayaTubeVideo
+        {
+            private int id;
+            string title;
+            private int playCount;
+
+            public SayaTubeVideo(string title)
+            {
+                var rand = new Random();
+                this.id = rand.Next(9999);
+                this.title = title;
+                this.playCount = 0;
+            }
+
+            public void IncreasePlayCount(int jmlh)
+            {
+                playCount += jmlh;
+            }
+
+            public void PrintVideoDetails()
+            {
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Title: " + title);
+                Console.WriteLine("Play count: " + playCount);
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            SayaTubeVideo vid1 = new SayaTubeVideo("Tutorial Design By Contract – Marjauza Naswansyah");
+            vid1.IncreasePlayCount(1000);
+            vid1.PrintVideoDetails();
+            Console.ReadLine();
         }
     }
 }
